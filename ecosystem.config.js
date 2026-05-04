abb0337222093715c5c118df5e4adf30c9daf233
root <root@srv1209731.hstgr.cloud>
2026-05-04 06:15:26 +0000

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'malaysia-backend',
+      script: 'dist/src/main.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 9003,
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true,
+    },
+  ],
+};
